8fc456abe342a920202829741e5b8717f5b5e6a4
danielamram <danielamram0@gmail.com>
2024-01-02 23:05:11 +0700

    fix(instructor): cleanup

--- src/screens/Main/Tabs/Home/Instructor.tsx
+++ src/screens/Main/Tabs/Home/Instructor.tsx
@@ -1,49 +1,29 @@
 import MeditationItem from '@common/components/MeditationItem';
 import ParallaxScrollView from '@common/components/ParallaxScrollView';
 import theme from '@common/theme';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 import useInstructor from '@services/hooks/useInstructor';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { FlatList, Linking, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { ScaledSheet } from 'react-native-size-matters';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { useSelector } from 'react-redux';
-import { allMeditations } from 'store/selectors';
-import styled from 'styled-components';
-
-const scaledSheet = ScaledSheet.create({
-  resFont: {
-    fontSize: '14@ms0.5',
-  },
-});
-
-const ButtonContainer = styled.View`
-  margin: 10px 23px 0px;
-`;
+import { allMeditations as allMeditationsSelector } from 'store/selectors';
 
 const Instructor = () => {
-  const { goBack } = useNavigation();
   const route = useRoute();
 
   const instructorId = route.params?.id;
   const instructor = useSelector(state => state.appData.instructors).find(
     x => x._id === instructorId,
   );
-  // const categories = useSelector(categoriesSelector);
-  const allMeditation = useSelector(allMeditations);
-  const dataArray = [];
-  instructor.categories.map((item, index) => {
-    let data;
-    const findmed = allMeditation.find(medId => medId.id === item);
 
-    if (findmed) {
-      data = {
-        ...findmed,
-      };
-      dataArray.push(data);
-    }
-  });
+  const allMeditations = useSelector(allMeditationsSelector);
+
+  const meditations = useMemo(
+    () => allMeditations.filter(({ id }) => instructor.categories.includes(id)),
+    [allMeditations, instructor.categories],
+  );
 
   const { updateIstructorTractionData } = useInstructor();
 
@@ -60,25 +40,6 @@ const Instructor = () => {
         <View className="flex flex-row justify-between items-center mt-2">
           <Text className="text-2xl font-bold">{instructor?.name}</Text>
           <View className="flex flex-row space-x-2">
-            {instructor.buttonLink && (
-              <TouchableOpacity
-                onPress={() => {
-                  const data = {
-                    ...instructor,
-                    button_press: true,
-                  };
-                  updateIstructorTractionData(data);
-                  const url = instructor.buttonLink;
-                  Linking.canOpenURL(url);
-                  Linking.openURL(url);
-                }}>
-                <Icon
-                  name="instagram"
-                  size={28}
-                  color={theme.colors.textColor}
-                />
-              </TouchableOpacity>
-            )}
             {instructor.SocialIconLink && (
               <TouchableOpacity
                 onPress={() => {
@@ -91,6 +52,25 @@ const Instructor = () => {
                   Linking.canOpenURL(url);
                   Linking.openURL(url);
                 }}>
+                <Icon
+                  name="instagram"
+                  size={28}
+                  color={theme.colors.textColor}
+                />
+              </TouchableOpacity>
+            )}
+            {instructor.buttonLink && (
+              <TouchableOpacity
+                onPress={() => {
+                  const data = {
+                    ...instructor,
+                    button_press: true,
+                  };
+                  updateIstructorTractionData(data);
+                  const url = instructor.buttonLink;
+                  Linking.canOpenURL(url);
+                  Linking.openURL(url);
+                }}>
                 <Icon name="link" size={28} color={theme.colors.textColor} />
               </TouchableOpacity>
             )}
@@ -99,12 +79,9 @@ const Instructor = () => {
         <Text className="text-left text-base leading-none font-normal mt-4">
           {instructor?.description}
         </Text>
-        <View>
-          <ButtonContainer />
-        </View>
       </View>
       <Text className="text-center text-base leading-none font-normal mt-2 text-gray-500">
-        {dataArray.length} מדיטציות
+        {meditations.length} מדיטציות
       </Text>
       <FlatList
         contentContainerStyle={{
@@ -112,7 +89,7 @@ const Instructor = () => {
           paddingVertical: 10,
           paddingHorizontal: 10,
         }}
-        data={dataArray}
+        data={meditations}
         scrollEnabled={false}
         keyExtractor={item => item.id}
         renderItem={renderMeditationItem}
