eea152edccadd417ec232ddba9cea93ff8aa3dc0
danielamram <danielamram0@gmail.com>
2024-01-05 16:04:47 +0700

    fix(analytics): simplified events

--- src/services/hooks/useAmplitude.ts
+++ src/services/hooks/useAmplitude.ts
@@ -6,19 +6,23 @@ import {
   setUserId,
   track,
 } from '@amplitude/analytics-react-native';
+import { useCallback } from 'react';
 
 const AMPLITUDE_API_KEY = '99c650de6e3028a4c95fab92b1b9ea7d';
 
 init(AMPLITUDE_API_KEY);
 
 export const useAmplitude = () => {
+  const trackEvent = useCallback(
+    (event: string, props?: Record<string, any>) => {
+      track(event, props);
+    },
+    [],
+  );
+
   return {
-    logEvent: event => {
-      track(event);
-    },
-    setUserId: userId => {
-      setUserId(userId);
-    },
+    logEvent: trackEvent,
+    setUserId,
     logRevenue: item => {
       try {
         const event = new Revenue()
