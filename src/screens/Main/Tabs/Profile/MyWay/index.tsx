3ac65c5e172fe78672a5af0e5f3c56f3bb4f3a77
danielamram <danielamram0@gmail.com>
2024-01-05 15:52:48 +0700

    fix(myWay): add titles & badges ui fix

--- src/screens/Main/Tabs/Profile/MyWay/index.tsx
+++ src/screens/Main/Tabs/Profile/MyWay/index.tsx
@@ -1,10 +1,20 @@
 import Divider from '@common/components/Divider';
+import { SubTitle } from '@common/components/Styled';
 import React from 'react';
+import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import styled from 'styled-components';
 
 import Badges from './Badges';
 import MyCollections from './MyCollections';
 import UserMetrics from './UserMetrics';
+
+const Title = styled(SubTitle)`
+  font-size: 22px;
+  font-weight: bold;
+  align-self: flex-start;
+  margin-bottom: 10px;
+`;
 
 const MyWay = () => (
   <ScrollView
@@ -13,8 +23,9 @@ const MyWay = () => (
     contentContainerStyle={{
       marginTop: 20,
     }}>
+    <Title className="ml-4" t="הרגעים שלי" />
     <UserMetrics />
-    <Divider className="my-6" />
+    <View className="my-3" />
     <Badges />
     <Divider className="my-6" />
     <MyCollections />
