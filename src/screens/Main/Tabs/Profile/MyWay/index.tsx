7970585d6533b2df77073b7e9b00774da64dd482
danielamram <danielamram0@gmail.com>
2024-02-03 14:52:19 +0700

    fix(myway): quote color style

--- src/screens/Main/Tabs/Profile/MyWay/index.tsx
+++ src/screens/Main/Tabs/Profile/MyWay/index.tsx
@@ -74,7 +74,7 @@ const MyWay = ({ navigation }) => {
           />
         </View>
         <View className="px-5 mt-16">
-          <Text className="mt-4 mb-6 text-center text-lg italic font-medium">
+          <Text className="mt-4 mb-6 text-center text-lg italic font-medium text-black">
             - "{getRandomElementsByDay<string>(quoteData, 1)[0]}"
           </Text>
           <Title className="mb-5" t="הרגעים שלי" />
