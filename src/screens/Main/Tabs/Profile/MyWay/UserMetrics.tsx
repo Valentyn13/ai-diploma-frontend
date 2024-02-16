550f660e888ac25da07ccd140b975d952cb46d0c
danielamram <danielamram0@gmail.com>
2024-02-16 19:31:51 +0700

    fix(myway): update stats ui

--- src/screens/Main/Tabs/Profile/MyWay/UserMetrics.tsx
+++ src/screens/Main/Tabs/Profile/MyWay/UserMetrics.tsx
@@ -1,3 +1,4 @@
+import { fonts } from '@common/theme';
 import i18n from '@services/localization/i18n';
 import React from 'react';
 import { Text, View } from 'react-native';
@@ -5,10 +6,18 @@ import { useSelector } from 'react-redux';
 
 const MetricBox = ({ value, title, ...props }) => (
   <View
-    className="flex flex-1 text-center h-24 justify-center items-center text-white bg-[#513F73] rounded-lg"
+    className="flex flex-1 text-center h-24 justify-center items-center text-white rounded-lg"
     {...props}>
-    <Text className="text-3xl font-bold text-white text-left">{value}</Text>
-    <Text className="text-lg text-white text-left">{title}</Text>
+    <Text
+      style={{ fontFamily: fonts?.regular }}
+      className="text-4xl font-bold text-black text-left">
+      {value}
+    </Text>
+    <Text
+      style={{ fontFamily: fonts?.regular }}
+      className="text-xl text-black text-left">
+      {title}
+    </Text>
   </View>
 );
 
