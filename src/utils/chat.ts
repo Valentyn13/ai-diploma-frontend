45957889b0169e84c237194a76385f4f2ec574cc
danielamram <danielamram0@gmail.com>
2024-02-17 12:06:54 +0700

    fix(chat): update quick replies

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -55,10 +55,7 @@ export const FIRST_MESSAGES: IMessage[] = [
 ];
 
 export function removeEmojiesFromString(str: string) {
-  return str.replace(
-    /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{1FB00}-\u{1FBFF}\u{1FC00}-\u{1FCFF}]/gu,
-    '',
-  );
+  return str.replace(/🚀|✨|🧠|🆙/g, '');
 }
 
 export const mapMessageToIMessage = (message: Message): IMessage => {
