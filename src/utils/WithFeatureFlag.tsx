c92ac978cdbd24aa3c059798a3fdb7889d779d15
danielamram <danielamram0@gmail.com>
2024-04-08 12:51:32 +0700

    chore(deps): remove flagsmith

--- src/utils/WithFeatureFlag.tsx
+++ src/utils/WithFeatureFlag.tsx
@@ -1,30 +1,28 @@
 import { useUser } from '@services/hooks/useUser';
-import flagsmith from 'flagsmith';
-import { FlagsmithProvider } from 'flagsmith/react';
-import React, { FC, PropsWithChildren, useEffect } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 
 const PROJECT_ID = 'D54TcUEfUjYyqMRNjX2bLu';
 
 const WithFeatureFlag: FC<PropsWithChildren> = ({ children }) => {
   const { user } = useUser();
 
-  useEffect(() => {
-    if (user.id) {
-      flagsmith.identify(user.id);
-    } else {
-      flagsmith.logout();
-    }
-  }, [user.id]);
+  // useEffect(() => {
+  //   if (user.id) {
+  //     flagsmith.identify(user.id);
+  //   } else {
+  //     flagsmith.logout();
+  //   }
+  // }, [user.id]);
 
   return (
-    <FlagsmithProvider
-      options={{
-        environmentID: PROJECT_ID,
-        cacheFlags: true,
-      }}
-      flagsmith={flagsmith}>
-      <>{children}</>
-    </FlagsmithProvider>
+    // <FlagsmithProvider
+    //   options={{
+    //     environmentID: PROJECT_ID,
+    //     cacheFlags: true,
+    //   }}
+    //   flagsmith={flagsmith}>
+    <>{children}</>
+    // </FlagsmithProvider>
   );
 };
 
