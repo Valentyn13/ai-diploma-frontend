46c1894e965ffd3dc4fda59e6ac0e19e2cd76336
Lev Alieshchenko <shallerglases@gmail.com>
2025-02-03 18:01:14 +0200

    chore: update base api url

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,6 +1,6 @@
-import { DEV_HOST, HOST, PORT } from '@env';
+import { DEV_HOST, HOST } from '@env';
 
-const baseURL = `http://${HOST}:${PORT}/v1/`;
+const baseURL = `https://${HOST}/v1/`;
 const isDev = process.env.NODE_ENV === 'development';
 
 export default {
