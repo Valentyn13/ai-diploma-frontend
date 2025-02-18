684ae398674e6edf7ba56e045927aead2aa37789
Valentyn13 <valentyn13git@gmail.com>
2025-02-18 19:09:04 +0200

    ref: make app ltr

--- android/app/src/main/java/com/rega/regaapp/MainApplication.java
+++ android/app/src/main/java/com/rega/regaapp/MainApplication.java
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
 //     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
 
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
-    sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
-    sharedI18nUtilInstance.forceRTL(getApplicationContext(), true);
+    sharedI18nUtilInstance.allowRTL(getApplicationContext(), false);
+    sharedI18nUtilInstance.forceRTL(getApplicationContext(), false);
   }
 }
